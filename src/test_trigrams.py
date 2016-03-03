e1acfa9c65eb6e2e2257fa673f6f7ab5c30cf1f7
Joseph McClenahan <jmcclena94@gmail.com>
2016-03-02 21:24:35 -0800

    linted test_trigrams.py

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -3,11 +3,12 @@
 import pytest
 
 TEXT_TABLE = [
-    ('dummy_text.txt', 'My favorite color is red. His favorite color is blue.  The baloons color is green.')
+    ('dummy_text.txt', 'My favorite color is red. His favorite color is blue.'
+        '  The baloons color is green.')
 ]
 
 CLEAN_TABLE = [
-	('\r\nwhat up?  Double it.', ' what up? Double it.')
+    ('\r\nwhat up?  Double it.', ' what up? Double it.')
 ]
 
 DIC_TABLE = [
@@ -15,9 +16,8 @@ DIC_TABLE = [
 ]
 
 MAKE_TABLE = [
-	(8,{'what up?': ['Double'], 'up? Double': ['it.']}, 8)
+    (8, {'what up?': ['Double'], 'up? Double': ['it.']}, 8)
 ]
-
 
 
 @pytest.mark.parametrize('path, result', TEXT_TABLE)
@@ -30,7 +30,6 @@ def test_text_import(path, result):
 def test_clean_read(full_text, result):
     from trigrams import clean_read
     assert clean_read(full_text) == result
-    
 
 
 @pytest.mark.parametrize('clean_text, result', DIC_TABLE)
@@ -38,8 +37,8 @@ def test_create_dic(clean_text, result):
     from trigrams import create_dic
     assert create_dic(clean_text) == result
 
+
 @pytest.mark.parametrize('num, dic, result', MAKE_TABLE)
 def test_make_story(num, dic, result):
     from trigrams import make_story
     assert len(make_story(num, dic)) == result
-    
